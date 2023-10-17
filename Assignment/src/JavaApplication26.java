5463f3e972c4e413e6a285c0df33b4227eacc36f
Darwinology15 <148201179+Darwinology15@users.noreply.github.com>
2023-10-17 18:38:23 +0800

    Add files via upload

---
+++ Assignment/src/JavaApplication26.java
@@ -0,0 +1,39 @@
+import  java.util.Scanner;
+
+public class JavaApplication26 {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your name:");
+        String nam = scanner.nextLine();
+        
+        System.out.print("Enter your Course:");
+        String co = scanner.nextLine();
+        
+        System.out.print("Enter your Age:");
+        String age = scanner.nextLine();
+        
+        System.out.print("Enter your Subject:");
+        String sub = scanner.nextLine();
+        
+        System.out.print("Enter your Address:");
+        String add = scanner.nextLine();
+        
+        System.out.print("Enter your Birthday:");
+        String birth = scanner.nextLine();
+        
+        System.out.print("Enter your Cellphone No.:");
+        String cellNo = scanner.nextLine();
+        
+        System.out.print("Welcome " + nam + " to NTC!");
+        
+        
+       
+        
+        
+
+    }
+    
+}
